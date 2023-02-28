c419da10b165fa164498ab158313d65f94308819
Ashok Pudasaini <67636572+ashokpds15@users.noreply.github.com>
2023-02-28 16:02:41 +0545

    Update Home2.js
    .

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -24,7 +24,7 @@ function Home2() {
               <br />
               <br />I am programmer in classics like
               <i>
-                <b className="purple"> Python, React and Javascript. </b>
+                <b className="purple"> Python, C++ and Javascript. </b>
               </i>
               <br />
               <br />
