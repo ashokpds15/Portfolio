08613dbde8e9c09bbedd0dcaa1ee4db65daf3a42
Ashok Pudasaini <67636572+ashokpds15@users.noreply.github.com>
2023-03-08 08:03:13 +0000

    add new project link

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -48,9 +48,9 @@ function Projects() {
               imgPath={editor}
               isBlog={false}
               title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              //ghLink="https://github.com/soumyajit4419/Editor.io"
-              //demoLink="https://editor.soumya-jit.tech/"              
+              description="Chat with GPT is an open-source, unofficial ChatGPT app with extra features and more ways to customize your experience. It connects ChatGPT with ElevenLabs to give ChatGPT a realistic human voice."
+              https://github.com/ashokpds15/Own-Chat-GPT
+              demoLink="https://ashokpds15.github.io/own-chat-gpt/          
             />
           </Col>
 
